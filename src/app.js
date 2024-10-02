c1b8778277ed49fdaf95e1306738117f87ce7037
Orangel Hernandez <orangel.hernandez1999@gmail.com>
2024-10-02 19:29:47 +0000

    feat: added an excuse generator using Javascript

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +5,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const sujeto = [
+    "My Parrot",
+    "a Dolphin",
+    "My Sister in Law",
+    "The Chupacabra"
+  ];
+  const accion = ["Spit on", "Burned", "Smashed", "Forgot"];
+  const objeto = ["My homework", "My laptop", "Mouse", "Bag"];
+  const tiempo = [
+    "Yesterday",
+    "Some days ago",
+    "Last year",
+    "Just before I came here"
+  ];
+
+  const getrandom = arr => {
+    let result = arr[Math.floor(Math.random() * arr.length)];
+    return result;
+  };
+  const excuseLink = document.querySelector("#excuse");
+
+  const excuse = `${getrandom(sujeto)} 
+  ${getrandom(accion)} 
+  ${getrandom(objeto)} 
+  ${getrandom(tiempo)}`;
+
+  excuseLink.innerHTML = excuse;
 };
